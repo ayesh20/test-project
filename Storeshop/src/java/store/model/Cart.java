0906c547e85eee7886ec46ad7b1a8daaf190ee5f
yasindu dahanayaka <yasindudahanayake@gmail.com>
2024-04-30 21:03:51 +0530

    add java classes

---
+++ Storeshop/src/java/store/model/Cart.java
@@ -0,0 +1,38 @@
+package store.model;
+
+public class Cart extends Product {
+
+    private int productId; // Adding productId field
+
+    private int quantity;
+    private double total;
+
+    public Cart() {
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    // Implementing getProductId method
+    public int getProductId() {
+        return productId;
+    }
+
+    // Setter for productId
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+}
